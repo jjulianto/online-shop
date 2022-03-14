2dbca1eb114fa353233f8bb6cf5c81aab51fd76f
jjulianto <jjulianto08@gmail.com>
2022-03-14 22:34:01 +0700

    chore: configure global functions with mixins

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+//define mixins for global function
+app.mixin({
+    methods: {
+        //money thousands
+        moneyFormat(number) {
+            let reverse = number.toString().split('').reverse().join(''),
+                thousands = reverse.match(/\d{1,3}/g)
+            thousands = thousands.join('.').split('').reverse().join('')
+            return thousands
+        },
+
+        //calculate discount
+        calculateDiscount(product) {
+            return product.price - (product.price * (product.discount) / 100)
+        }
+    }
+})
+
+app.mount('#app')
