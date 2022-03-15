66ecca1941711553fb0d4a148aa2e3b99b272120
jjulianto <jjulianto08@gmail.com>
2022-03-15 21:01:11 +0700

    chore: configuration global api endpoint

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@ import App from './App.vue'
 
 const app = createApp(App)
 
-//define mixins for global function
 app.mixin({
     methods: {
-        //money thousands
         moneyFormat(number) {
             let reverse = number.toString().split('').reverse().join(''),
                 thousands = reverse.match(/\d{1,3}/g)
@@ -14,7 +12,6 @@ app.mixin({
             return thousands
         },
 
-        //calculate discount
         calculateDiscount(product) {
             return product.price - (product.price * (product.discount) / 100)
         }
