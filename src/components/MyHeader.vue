5a050f8beb8c3d4b309c1425f06e74a91c69f7a3
jjulianto <jjulianto08@gmail.com>
2022-03-15 20:52:04 +0700

    chore: create component header and footer

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,42 @@
+<template>
+    <header class="section-header top-0">
+        <section class="header-main">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-md-3 col-7">
+                        <a href="#" class="text-decoration-none" data-abc="true">
+                            <span class="logo"><i class="fa fa-apple-alt"></i> Apple Store </span></a>
+                    </div>
+                    <div class="col-md-5 d-none d-md-block">
+                        <form class="search-wrap">
+                            <div class="input-group w-100"><input type="text" class="form-control search-form"
+                                    style="width:55%;border: 1px solid #ffffff" name="q"
+                                    placeholder="Mau beli apa hari ini ?">
+                                <div class="input-group-append">
+                                    <button class="btn search-button" type="submit"><i class="fa fa-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-4 col-5">
+                        <div class="d-flex justify-content-end">
+                            <div class="cart-header">
+                                <a href="#" class="btn search-button btn-md" style="color: #ffffff;background-color: #6677ef;border-color: #ffffff;"><i class="fa fa-shopping-cart"></i> 0 | Rp. 0 </a>
+                            </div>
+                            <div class="account">
+                                <a href="#" class="btn search-button btn-md d-none d-md-block ms-4"><i class="fa fa-user-circle"></i> ACCOUNT</a>    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'MyHeader',
+    }
+</script>
